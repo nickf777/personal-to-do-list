dc285d4843aa0da25c6a14b839f53e84df24c574
Nicholas Faller <nickfallerguitar@gmail.com>
2019-10-08 12:33:16 -0400

    Addition of to-do list functionality

--- script.js
+++ script.js
@@ -13,18 +13,43 @@ document.getElementById("add").addEventListener("click", function() {
     let value = document.getElementById("item").value;  
     if (value) {
       addItemTodo(value);
+    } else {
+      alert("What do you have to do today?")
     }
     document.getElementById("item").value = "";
 })
 
 function addItemTodo(text) {
   let item = document.createElement("li");
+  item.classList.add("listitem")
   item.innerText = text;
+
   let list = document.getElementById("todo");
   list.appendChild(item);
+
+  let completeButton = document.createElement("button");
+  completeButton.classList.add("done");
+  item.appendChild(completeButton);
+
+  //Completing a task on clikc
+
+  completeButton.addEventListener("click", moveToComplete)
+
+  let completeButtonImage = document.createElement("img");
+  completeButtonImage.classList.add("completeimage")
+  completeButton.appendChild(completeButtonImage);
+
   remaining++;
-
-
-
-
 }
+
+//Completing a Task
+function moveToComplete() {
+  let item = this.parentNode;
+  let parent = this.parentNode.parentNode;
+  let completeList = document.getElementById("complete_list");
+
+  parent.removeChild(item);
+  completeList.appendChild(item);
+}
+
+
